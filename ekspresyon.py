4172d3d85d12f540b483c364548d9dec8a1c5ac7
burhanettiny <mailtoburhanettin@gmail.com>
2025-11-14 12:07:46 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -685,12 +685,18 @@ if st.session_state.show_guide:
                 position: relative;
             ">
                 <div style='position: absolute; top: 10px; right: 20px;'>
-    <button onclick="this.closest('div[style*=&quot;position: fixed&quot;]').style.display='none';"
-            style="background: none; border: none; font-size: 24px; cursor: pointer;">
-        ✖
-    </button>
-</div
-    
+                    <button onclick="this.closest('div[style*=&quot;position: fixed&quot;]').style.display='none';"
+                            style="background: none; border: none; font-size: 24px; cursor: pointer;">
+                        ✖
+                    </button>
+                </div>
+                {user_guide_en}
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+                    
 st.markdown(f"<h3>{translations[language_code]['title']}</h3>", unsafe_allow_html=True)
 st.markdown(f"<h4>{translations[language_code]['patient_data_header']}</h4>", unsafe_allow_html=True)
 
