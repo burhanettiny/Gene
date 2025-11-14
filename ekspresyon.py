fb02453041726ed33895d13f0f16307f35f24ef8
burhanettiny <mailtoburhanettin@gmail.com>
2025-11-14 12:05:59 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -689,7 +689,7 @@ if st.session_state.show_guide:
             style="background: none; border: none; font-size: 24px; cursor: pointer;">
         ✖
     </button>
-</div>
+</div
     
 st.markdown(f"<h3>{translations[language_code]['title']}</h3>", unsafe_allow_html=True)
 st.markdown(f"<h4>{translations[language_code]['patient_data_header']}</h4>", unsafe_allow_html=True)
